2dfe107c70ac9484c88fa65c5f2b732773667536
201561 <douglas.moeller@verdecard.com.br>
2020-12-10 17:55:25 -0300

    - Pergunta e botões adicionados; - Chamando função;

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,41 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+
+  void responder() {
+    print('Pergunta repsondida!');
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final perguntas = [
+      'Qual região do Brasil você mora?',
+      'Qual time você torce?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Primeiro app Flutter'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Norte'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Sul'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Nordeste'),
+              onPressed: responder,
+            ),
+          ],
+        ),
       )
     );
   }
